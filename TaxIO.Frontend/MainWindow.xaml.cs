8317378150082dbfa7719fd307b6d5a702b4ecc3
KikiZC <kikizc22@gmail.com>
2025-04-27 10:46:20 +0200

    Přidány stránky

--- TaxIO.Frontend/MainWindow.xaml.cs
+++ TaxIO.Frontend/MainWindow.xaml.cs
@@ -1,24 +1,45 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TaxIO.Frontend.Pages;
 
 namespace TaxIO.Frontend
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingGroup MainGroup = new();
+
         public MainWindow()
         {
+            Core.OnStart.Start();
+
             InitializeComponent();
+
+            MainPage.Content = new DashBoard();
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ToggleButton? toggleButton = sender as ToggleButton;
+            if (toggleButton == null) return;
+
+            string? tag = (string)toggleButton.Tag;
+
+            if (tag == null) return;
+
+            MainPage.Content = tag switch
+            {
+                "Dashboard" => new DashBoard(),
+                "Income" => new IncomePG(),
+                "Expense" => new ExpensePG(),
+                "Inventory" => new InventoryPG(),
+                "Charts" => new ChartPG(),
+                "Check" => new CheckPG(),
+                "Settings" => new SettingsPG(),
+                _ => new DashBoard(),
+            };
+
+
         }
     }
 }
